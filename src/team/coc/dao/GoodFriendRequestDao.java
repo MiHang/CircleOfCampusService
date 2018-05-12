403625b53283f702b68e3860143ad5519c7e49ed
惠普 <508530366@qq.com>
2018-05-12 16:16:51 +0800

    没什么想说的，MMP

--- src/team/coc/dao/GoodFriendRequestDao.java
+++ src/team/coc/dao/GoodFriendRequestDao.java
@@ -10,16 +10,62 @@ import team.coc.util.HibernateUtils;
 import java.util.List;
 
 /**
- * 用户表操作类
+ * 好友申请表操作类
  */
-public class GoodFriendRequestDao extends CommonDao<User> {
+public class GoodFriendRequestDao extends CommonDao<GoodFriendRequest> {
 
+    /**
+     * 保存好友申请表
+     * @param goodFriendRequest
+     * @return
+     */
+    @Override
+    public boolean save(GoodFriendRequest goodFriendRequest) {
+        goodFriendRequest.getUser1().setFaculty(null);
+        goodFriendRequest.getUser2().setFaculty(null);
+        return super.save(goodFriendRequest);
+    }
+    /**
+     * 通过账号检查是否存在此用户
+     * @param A_user - 账号(用户名/邮箱)
+     * @return 存在返回true， 不存在返回false
+     */
+    public boolean hasRequest(String A_user,String B_user) {
+
+        Session session = HibernateUtils.openSession();
+        Transaction tx = null;
+        List<Object> objectList;
+        try {
+            tx = session.beginTransaction();
+
+            // 查询数据库
+            String hql = " from GoodFriendRequest where  user1.email= ? and user2.email = ?";
+            Query query = session.createQuery(hql);
+            query.setParameter(0, A_user);
+            query.setParameter(1, B_user);
+            objectList = query.list();
+
+            tx.commit();
+        } catch (RuntimeException e) {
+            tx.rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
+
+        // 存在结果
+        if (objectList != null && objectList.size() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
     /**
      * 根据账号查询好友申请信息
      * @param account - 账号
      * @return List<GoodFriendRequest>
      */
-    public List<GoodFriendRequest> getUserByAccount(String account) {
+    public List<GoodFriendRequest> getRequest(String account) {
 
         Session session = HibernateUtils.openSession();
         Transaction tx = null;
@@ -47,80 +93,6 @@ public class GoodFriendRequestDao extends CommonDao<User> {
             return userList;
         } else {
             return null;
-        }
-    }
-
-    /**
-     * 验证用户身份
-     * @param account - 账号（邮箱/用户名）
-     * @param pwd - 密码
-     * @return 验证成功返回userId， 失败返回 -1
-     */
-    public int isValidity(String account, String pwd) {
-
-        Session session = HibernateUtils.openSession();
-        Transaction tx = null;
-        List<Object> objects;
-        try {
-            tx = session.beginTransaction();
-
-            // 查询数据库
-            String hql = "select userId from User where userName = ? or email = ? and pwd = ?";
-            Query query = session.createQuery(hql);
-            query.setParameter(0, account);
-            query.setParameter(1, account);
-            query.setParameter(2, pwd);
-            objects = query.list();
-
-            tx.commit();
-        } catch (RuntimeException e) {
-            tx.rollback();
-            throw e;
-        } finally {
-            session.close();
-        }
-
-        // 存在结果
-        if (objects != null && objects.size() > 0) {
-            return (int)objects.get(0);
-        } else {
-            return -1;
-        }
-    }
-
-    /**
-     * 通过账号检查是否存在此用户
-     * @param account - 账号(用户名/邮箱)
-     * @return 存在返回true， 不存在返回false
-     */
-    public boolean hasUser(String account) {
-
-        Session session = HibernateUtils.openSession();
-        Transaction tx = null;
-        List<Object> objectList;
-        try {
-            tx = session.beginTransaction();
-
-            // 查询数据库
-            String hql = "select userId from User where userName = ? or email = ?";
-            Query query = session.createQuery(hql);
-            query.setParameter(0, account);
-            query.setParameter(1, account);
-            objectList = query.list();
-
-            tx.commit();
-        } catch (RuntimeException e) {
-            tx.rollback();
-            throw e;
-        } finally {
-            session.close();
-        }
-
-        // 存在结果
-        if (objectList != null && objectList.size() > 0) {
-            return true;
-        } else {
-            return false;
         }
     }
 }
--- src/team/coc/test/Demo.java
+++ src/team/coc/test/Demo.java
@@ -5,6 +5,7 @@ import team.coc.dao.GoodFriendRequestDao;
 import team.coc.dao.UserDao;
 import team.coc.pojo.Campus;
 import team.coc.pojo.GoodFriendRequest;
+import team.coc.pojo.User;
 
 import java.util.Date;
 import java.util.List;
@@ -13,20 +14,33 @@ public class Demo {
     public static void main(String[]args) {
         //校园表示例数据
         UserDao dao=new UserDao();
-        CommonDao<GoodFriendRequest> campusCommonDao = new CommonDao<GoodFriendRequest>();
+
+        GoodFriendRequestDao goodFriendRequestDao = new GoodFriendRequestDao();
         GoodFriendRequest campus = new GoodFriendRequest();
         campus.setRequestTime(new Date());
         campus.setRequestReason("认识一下");
-        campus.setUser1(dao.getUserByAccount("1"));
-        campus.setUser2(dao.getUserByAccount("2"));
+        campus.setUser1(dao.getUserByAccount("87654321@qq.com"));
+        campus.setUser2(dao.getUserByAccount("jayevip@163.com"));
         campus.setResult(0);
-        campusCommonDao.save(campus);
+
 
         GoodFriendRequestDao goodFriendRequest=new GoodFriendRequestDao();
-        List<GoodFriendRequest> data=goodFriendRequest.getUserByAccount("1");
+       if(! goodFriendRequest.hasRequest("87654321@qq.com","jayevip@163.com")){
+           goodFriendRequestDao.save(campus);
+        }else{
+                   System.out.println("您已发起过申请信息");
+               }
+        List<GoodFriendRequest> data=goodFriendRequest.getRequest("jayevip@163.com");
         if (data!=null){
-
+            System.out.println(data.get(0).getUser1().getUserName()+"请求添加"+data.get(0).getUser2().getUserName()+"为好友");
+        }else{
+            System.out.println("无好友申请信息");
         }
+
+
+        //好友查询
+        List<User> l=dao.getRequest("ja");
+        System.out.println("好友查询"+l.get(0).getUserName());
     }
 
 }
