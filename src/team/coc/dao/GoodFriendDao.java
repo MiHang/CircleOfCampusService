3e55f2fe7456da47cc9a58e114ccf6e18faf6bd6
breakadmin <123taaitabutnow>
2018-06-03 17:50:19 +0800

    代码冲突合并

--- src/team/coc/dao/GoodFriendDao.java
+++ src/team/coc/dao/GoodFriendDao.java
@@ -16,6 +16,40 @@ import java.util.List;
  */
 public class GoodFriendDao extends CommonDao<GoodFriend> {
 
+    /**
+     * 通过ID获取GoodFriend对象
+     * @param id1
+     * @param id2
+     * @return
+     */
+    public GoodFriend getGoodFriendById(int id1, int id2) {
+        Session session = HibernateUtils.openSession();
+        Transaction tx = null;
+        List<GoodFriend> goodFriends;
+        try {
+            tx = session.beginTransaction();
+
+            // 查询数据库
+            String hql = "from GoodFriend where user1.id = ? and user2.id = ?";
+            Query query = session.createQuery(hql);
+            query.setParameter(0, id1);
+            query.setParameter(1, id2);
+            goodFriends = query.list();
+
+            tx.commit();
+        } catch (RuntimeException e) {
+            tx.rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
+
+        // 存在结果
+        if (goodFriends != null && goodFriends.size() > 0) {
+            return goodFriends.get(0);
+        }
+        return null;
+    }
 
     /**
      * 通过账号检查两人是否是好友
