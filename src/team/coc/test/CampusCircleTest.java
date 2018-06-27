d449adbc43888b14f8ab950c1925c1c376e61a43
Jaye Li <2238179368@qq.com>
2018-06-27 18:41:47 +0800

    导入数据库公告示例数据

--- src/team/coc/test/CampusCircleTest.java
+++ src/team/coc/test/CampusCircleTest.java
@@ -16,33 +16,62 @@ import java.util.concurrent.TimeUnit;
 public class CampusCircleTest {
     public static void main(String[] args) {
 
-        List<File> images1 = new ArrayList<File>();
-        File img1 = new File("D:\\AndroidTemp\\切图\\校园圈\\img_campus_kol.png");
-        images1.add(img1);
-        addCampusCircle(1,
-                "校园KOL 颜值担当 非你莫属",
-                "校园KOL 颜值担当 非你莫属",
-                "2018年5月2日 下午2:00",
-                "学术交流中心",
-                images1);
-
         List<File> images2 = new ArrayList<File>();
-        File img2 = new File("D:\\AndroidTemp\\切图\\校园圈\\img_sxcz.png");
+        File img2 = new File("D:\\AndroidTemp\\首页海报图\\2.png");
         images2.add(img2);
         addCampusCircle(1,
                 "\"书香成职\"系列活动成果展",
-                "\"书香成职\"系列活动成果展",
+                "    为了弘扬和传承中华优秀传统文化，培育和践行社会主义核心价值观，宣传贯彻党的十九大精神，营造浓浓的书香文化氛围， 4月23日下午，由学院图书馆主办的学院首届经典诵读大赛活动在高新校区财经分院学术报告厅举行。学院党委书记周鑑、党委副书记、院长王涛、党委副书记、纪委书记邹克俭、以及各分院部（处室）负责人、部分教师代表出席本次活动。\n" +
+                        "\t\n" +
+                        "    参加此次比赛的七支参赛队伍，全部是各分院初赛的胜出者。参赛节目有《大漠敦煌》、《满江红》、《沁园春•长沙》、《沁园春•雪》、《百年复兴 经典伴随》、《诵•离•合》、《星汉灿烂 若出其里》、《棋哥随手吟》。\n" +
+                        "\t\n" +
+                        "    经过激烈地竞争，最终财经分院、工房分院、旅游分院分别荣获荣获一、二、三等奖。其他分院获得优胜奖。至此，学院首届经典诵读大赛活动落下了帷幕。",
                 "2018年5月3日至10日",
                 "图书馆",
-                images1);
+                images2);
 
         List<File> images3 = new ArrayList<File>();
-        File img3 = new File("D:\\AndroidTemp\\切图\\校园圈\\img_sxcz.png");
+        File img3 = new File("D:\\AndroidTemp\\首页海报图\\3.png");
         images3.add(img3);
         addCampusCircle(1,
                 "\"五四青年节\"文艺汇演",
-                "\"五四青年节\"文艺汇演",
+                "一、汇演主题:拥抱青春 祝福明天\n" +
+                        "\n" +
+                        "二、汇演组织:\n" +
+                        "    1.主办：成都职业技术学院政教处\n" +
+                        "    2.承办：成都职业技术学院团委\n" +
+                        "    3.协办：成都职业技术学院各协会、社团\n" +
+                        "\t\n" +
+                        "三、汇演日程：\n" +
+                        "    1.演出时间：2018年5月4号 下午6:00\n" +
+                        "\t\n" +
+                        "四、汇演地点：学术交流中心",
                 "2018年5月4日 下午6:00",
+                "学术交流中心",
+                images3);
+
+        List<File> images1 = new ArrayList<File>();
+        File img1 = new File("D:\\AndroidTemp\\首页海报图\\1.png");
+        images1.add(img1);
+        addCampusCircle(1,
+                "校园KOL 颜值担当 非你莫属",
+                "招聘岗位：校园KOL \n" +
+                        "工作职责： \n" +
+                        "1、负责在本校中宣传国外留学相关咨询活动 \n" +
+                        "2、负责本校与本校团委学生会等学校组织建立合作关系 \n" +
+                        "\n" +
+                        "我们需要你有什么？ \n" +
+                        "1、超强的执行力和团队合作精神 \n" +
+                        "2、具有良好的人际沟通能力，善于与人沟通、打交道，勇于突破自己 \n" +
+                        "\n" +
+                        "你会收获什么？ \n" +
+                        "1、开具实习证明，表现优秀者可转正 \n" +
+                        "2、固定的岗位薪酬及提成 \n" +
+                        "3、定期与各大院校优秀KOL交流机会 \n" +
+                        "\n" +
+                        "时间：2018年5月2日下午2点\n" +
+                        "地点：学术交流中心",
+                "2018年5月2日 下午2:00",
                 "学术交流中心",
                 images1);
     }
--- src/team/coc/test/SocietyCircleTest.java
+++ src/team/coc/test/SocietyCircleTest.java
@@ -17,24 +17,58 @@ public class SocietyCircleTest {
     public static void main(String[] args) {
 
         List<File> images1 = new ArrayList<File>();
-        File img1 = new File("D:\\AndroidTemp\\切图\\校园圈\\img_badminton_club.png");
+        File img1 = new File("D:\\AndroidTemp\\首页海报图\\4.png");
         images1.add(img1);
         addSocietyCircle(1,
                 "羽毛球社团招新",
-                "羽毛球社团招新",
-                "2018年5月1日",
+                "    学弟学妹们大家好！这里是羽毛球社啦，首先说一句经典：羽毛球社欢迎你！(✿✪‿✪｡)ﾉ\n" +
+                        "    \n" +
+                        "\t简单介绍一下哈，羽毛球社呢，就是一个关于打羽毛球，了解羽毛球，喜欢羽毛球的一群人组成的一个社团。当然，如果你不了解它，也从未打过它（感觉我们在虐待羽毛球这种生物\"▔□▔），但是你想要去学习这个新技能那么这个社团的大门是为你敞开哒~\\(≧▽≦)/~！\n" +
+                        "\t\n" +
+                        "\t时间：2018年5月1日 下午5:00\n" +
+                        "    地点：高新篮球场",
+                "2018年5月6日",
                 "篮球场旁",
                 images1);
 
+        List<File> images3 = new ArrayList<File>();
+        File img3 = new File("D:\\AndroidTemp\\首页海报图\\6.png");
+        images3.add(img3);
+        addSocietyCircle(1,
+                "羽毛球社团周年庆",
+                "活动主题：“喜迎佳日,共渡羽毛球社周年庆”\n" +
+                        "\n" +
+                        "活动开展形式: 唱歌、跳舞、小品、相声、走秀等。\n" +
+                        "\n" +
+                        "活动时间：2018年06月03日 18:30 - 21:00\n" +
+                        "\n" +
+                        "活动地点：学术交流中心\n" +
+                        "\n" +
+                        "主办单位:成都职业技术学院学生羽毛球社",
+                "2018年06月03日 18:30",
+                "学术交流中心",
+                images3);
+
         List<File> images2 = new ArrayList<File>();
-        File img2 = new File("D:\\AndroidTemp\\切图\\校园官方公告\\competition_poster.png");
+        File img2 = new File("D:\\AndroidTemp\\首页海报图\\5.png");
         images2.add(img2);
         addSocietyCircle(1,
                 "激情绽放 \"羽\"你共享 第九届校园羽毛球比赛",
-                "激情绽放 \"羽\"你共享 第九届校园羽毛球比赛",
-                "2018年5月26日",
+                "一、竞赛时间：2018年06月20日\n" +
+                        "\n" +
+                        "二、竞赛地点：高新体育馆\n" +
+                        "\n" +
+                        "三、比赛项目：男子双打、女子双打、混合双打\n" +
+                        "\n" +
+                        "四、 报名办法\n" +
+                        "      1、男子双打、女子双打、混合双打各项目均可组队报名，不限报名队数，参赛选手不可兼项。\n" +
+                        "      2、报名时间与地点：\n" +
+                        "         各参赛队员于2018年06月12日12：00之前将报名名单交羽毛球社\n" +
+                        "      3、温馨提示：每个参赛队一定在赛前做适量的准备活动，避免运动受伤。",
+                "2018年06月20日",
                 "高新校区体育馆",
                 images2);
+
     }
 
     public static String addSocietyCircle(int uId,
